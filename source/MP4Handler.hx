575efc2c06638f08bc8961b2f2339bea2828338c
BushsHaxs <72326501+BushsHaxs@users.noreply.github.com>
2022-01-26 17:45:53 -0600

    Fixed mp4

--- source/MP4Handler.hx
+++ source/MP4Handler.hx
@@ -2,12 +2,12 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxState;
-import openfl.events.Event;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
 import openfl.media.Video;
 import openfl.net.NetConnection;
 import openfl.net.NetStream;
 import vlc.VlcBitmap;
-import flixel.FlxSprite;
 
 // THIS IS FOR TESTING
 // DONT STEAL MY CODE >:(
@@ -16,7 +16,7 @@ class MP4Handler
 	public static var video:Video;
 	public static var netStream:NetStream;
 	public static var finishCallback:FlxState;
-	public var sprite:FlxSprite;
+
 	#if desktop
 	public static var vlcBitmap:VlcBitmap;
 	#end
@@ -31,19 +31,28 @@ class MP4Handler
 		}
 	}
 
-	public function playMP4(path:String, callback:FlxState, ?outputTo:FlxSprite = null, ?repeat:Bool = false, ?isWindow:Bool = false, ?isFullscreen:Bool = false):Void
+	public function playMP4(path:String, callback:FlxState, ?repeat:Bool = false, ?isWindow:Bool = false, ?isFullscreen:Bool = false):Void
 	{
-		#if html5
-		FlxG.autoPause = false;
-
-		if (FlxG.sound.music != null)
-		{
-			FlxG.sound.music.stop();
-		}
-
 		finishCallback = callback;
 
-		video = new Video();
+                #if desktop
+		vlcBitmap = new VlcBitmap();
+		vlcBitmap.onVideoReady = onVLCVideoReady;
+		vlcBitmap.onComplete = onVLCComplete;
+		vlcBitmap.volume = FlxG.sound.volume;
+
+		if (repeat)
+			vlcBitmap.repeat = -1;
+		else
+			vlcBitmap.repeat = 0;
+
+		vlcBitmap.inWindow = isWindow;
+		vlcBitmap.fullscreen = isFullscreen;
+
+		FlxG.addChildBelowMouse(vlcBitmap);
+		vlcBitmap.play(checkFile(path));
+                #elseif html5
+                video = new Video();
 		video.x = 0;
 		video.y = 0;
 
@@ -57,52 +66,16 @@ class MP4Handler
 
 		nc.addEventListener("netStatus", netConnection_onNetStatus);
 
-		netStream.play(path);
-		#else
-		finishCallback = callback;
-
-		vlcBitmap = new VlcBitmap();
-
-		vlcBitmap.set_height(FlxG.stage.stageHeight);
-		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
-
-		trace("Setting width to " + FlxG.stage.stageHeight * (16 / 9));
-		trace("Setting height to " + FlxG.stage.stageHeight);
-
-		vlcBitmap.onVideoReady = onVLCVideoReady;
-		vlcBitmap.onComplete = onVLCComplete;
-		vlcBitmap.onError = onVLCError;
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, update);
-
-		if (repeat)
-			vlcBitmap.repeat = -1;
-		else
-			vlcBitmap.repeat = 0;
-
-		vlcBitmap.inWindow = isWindow;
-		vlcBitmap.fullscreen = isFullscreen;
-
-		FlxG.addChildBelowMouse(vlcBitmap);
-		vlcBitmap.play(checkFile(path));
-		if (outputTo != null)
-		{
-			// lol this is bad kek
-			
-			vlcBitmap.x = -20000; // defn off screen
-
-			sprite = outputTo;
-			trace("gosh darn rabitygs");
-		}
-		#end
+		netStream.play(videoPath);
+                #end
+    
 	}
 
-	#if desktop
+        #if desktop
 	function checkFile(fileName:String):String
 	{
 		var pDir = "";
 		var appDir = "file:///" + Sys.getCwd() + "/";
-
 		if (fileName.indexOf(":") == -1) // Not a path
 			pDir = appDir;
 		else if (fileName.indexOf("file://") == -1 || fileName.indexOf("http") == -1) // C:, D: etc? ..missing "file:///" ?
@@ -116,20 +89,10 @@ class MP4Handler
 	function onVLCVideoReady()
 	{
 		trace("video loaded!");
-		if (sprite != null)
-		{
-			sprite.loadGraphic(vlcBitmap.bitmapData);	
-			trace("loaded da graphics " + vlcBitmap.bitmapData);
-		}
 	}
 
-	public function onVLCComplete()
+	function onVLCComplete()
 	{
-		if (sprite != null)
-		{
-			vlcBitmap.pause();
-			return;
-		}
 		vlcBitmap.stop();
 
 		// Clean player, just in case!
@@ -144,27 +107,14 @@ class MP4Handler
 
 		if (finishCallback != null)
 		{
-			LoadingState.loadAndSwitchState(finishCallback);
+			FlxG.switchState(finishCallback);
 		}
-	}
-
-	function onVLCError()
-	{
-		if (finishCallback != null)
-		{
-			LoadingState.loadAndSwitchState(finishCallback);
-		}
-	}
-
-	function update(e:Event)
-	{
-		vlcBitmap.volume = FlxG.sound.volume; // shitty volume fix
 	}
 	#end
 
 	/////////////////////////////////////////////////////////////////////////////////////
 
-	function client_onMetaData(path)
+	function client_onMetaData(videoPath)
 	{
 		video.attachNetStream(netStream);
 
@@ -172,9 +122,9 @@ class MP4Handler
 		video.height = FlxG.height;
 	}
 
-	function netConnection_onNetStatus(path)
+	function netConnection_onNetStatus(videoPath)
 	{
-		if (path.info.code == "NetStream.Play.Complete")
+		if (videoPath.info.code == "NetStream.Play.Complete")
 		{
 			finishVideo();
 		}
@@ -191,23 +141,9 @@ class MP4Handler
 
 		if (finishCallback != null)
 		{
-			LoadingState.loadAndSwitchState(finishCallback);
+			FlxG.switchState(finishCallback);
 		}
 		else
-			LoadingState.loadAndSwitchState(new MainMenuState());
+			FlxG.switchState(new MainMenuState());
 	}
-
-	// old html5 player
-	/*
-		var nc:NetConnection = new NetConnection();
-		nc.connect(null);
-		var ns:NetStream = new NetStream(nc);
-		var myVideo:Video = new Video();
-		myVideo.width = FlxG.width;
-		myVideo.height = FlxG.height;
-		myVideo.attachNetStream(ns);
-		ns.play(path);
-		return myVideo;
-		ns.close();
-	 */
 }
